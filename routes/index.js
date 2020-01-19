c7da3a323d230b5287d2aed83ab4358a4b2c9214
Strapak, Sergej <strapak@mediamarktsaturn.com>
2020-01-19 10:34:30 +0100

    Tests

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router  = express.Router();
-const User = require("../models/User");
+//const User = require("../models/User");
 
-/* GET home page */
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/*
 router.get("/", (req, res, next) => {
   const loggedIn = req.session.user;
   User.find()
@@ -13,5 +18,5 @@ router.get("/", (req, res, next) => {
   .catch(error => {next(error)}
   );
 });
-
+*/
 module.exports = router;
