79838abcd1538a01ee1f8cf97dbc355f8719f525
Strapak, Sergej <strapak@mediamarktsaturn.com>
2020-01-20 16:35:03 +0100

    user session

--- routes/private/main_private.js
+++ routes/private/main_private.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/user')
 
+
+
 router.get('/', (req, res) => {
-    res.render('authorized/main', {User: User});
+  if(req.session.user) {
+    res.render('authorized/main', {username: req.session.user.username});
+  } else {
+    res.redirect('/login');
+  }
 });
 
 module.exports = router;
